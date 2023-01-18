6b6a358fce8d289df51ee6cce0f8ddc0ff5f0346
aimaduddin <aima.aimaduddin@gmail.com>
2023-01-18 20:01:45 +0800

    Eloquent Model Conventions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // protected $table = 'posts'; this is to let the model use custom name table
+
+    // protected $primaryKey = ''; custom primary key
+
+    // protected $timestamps = false; to disable the timestamps inside database
+
+    // protected $dateTime = 'U'; change timestamps format in db
+
+    // protected $attributes = [ defining default value for attributes
+    //     'is_published' => true
+    // ]
 }
