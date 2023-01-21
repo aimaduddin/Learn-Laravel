9b2b58227785feea5718394b04da7c4aa24e53ad
aimaduddin <aima.aimaduddin@gmail.com>
2023-01-21 11:05:30 +0800

    How to Use Form Requests in Laravel

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostFormRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -62,7 +63,7 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostFormRequest $request)
     {
         // OOP Way
         // $post = new Post();
@@ -74,13 +75,7 @@ class PostsController extends Controller
         // $post->is_published = $request->is_published === 'on';
         // $post->save();
 
-        $request->validate([
-            'title' => 'required|unique:posts|max:255',
-            'excerpt' => 'required',
-            'body' => 'required',
-            'image' => ['required', 'mimes:jpg,jpeg,png', 'max:5048'],
-            'min_to_read' => 'required|min:0|max:60',
-        ]);
+        $request->validated();
 
         Post::create([
             'title' => $request->title,
@@ -128,15 +123,9 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostFormRequest $request, $id)
     {
-        $request->validate([
-            'title' => 'required|max:255|unique:posts,title,' . $id,
-            'excerpt' => 'required',
-            'body' => 'required',
-            'image' => ['mimes:jpg,jpeg,png', 'max:5048'],
-            'min_to_read' => 'required|min:0|max:60',
-        ]);
+        $request->validated();
 
         Post::where('id', $id)->update(
             $request->is_published == 'on'
